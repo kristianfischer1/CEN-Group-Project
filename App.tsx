a5f8725445439a1eeb167774d912db11f8c5f20f
kristianfischer1 <116751910+kristianfischer1@users.noreply.github.com>
2024-04-09 10:55:13 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Login from "./src/Login";
+import { useCallback, useEffect, useState } from 'react';
+import { User, getAuth, onAuthStateChanged } from "firebase/auth";
+import "./firebaseConfig"
+import { UserContextProvider } from './src/providers/UserProvider';
+import TabNavigator from './src/navigation/TabNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+
+type UserState = User | undefined;
+
+export default function App() {
+  const [user, setUser] = useState<UserState>(undefined);
+  const [authChecked, setAuthChecked] = useState(false);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setAuthChecked(true);
+    });
+  
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  
+  if (!authChecked) return null;
+  if (user === null) return <Login setUser={setUser} />
+
+  return (
+    <NavigationContainer>
+      <UserContextProvider user={user}>
+        <TabNavigator/>
+      </UserContextProvider>
+    </NavigationContainer>
+  );
+}
+
---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCi4QjJ2A4rOQP4jpILuna-vzr0aXjEkEg",
+  authDomain: "nutrivision-33846.firebaseapp.com",
+  projectId: "nutrivision-33846",
+  storageBucket: "nutrivision-33846.appspot.com",
+  messagingSenderId: "535842695202",
+  appId: "1:535842695202:web:60de14d029c67fd54d427b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+  })
+  
+const db = getFirestore(app);
